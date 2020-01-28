1c08e75d20a2b4c166c471eb94ae13409e349fa0
Humay <crypto347@gmail.com>
2020-01-28 12:57:27 +0400

    code cleaning

--- src/components/main.js
+++ src/components/main.js
@@ -24,16 +24,6 @@ import './main.scss';
 * Components
 */
 
-import Toolbar from './Parts/Toolbar/toolbar';
-import MovingBubbles from './Parts/MovingBubbles/movingBubbles';
-import OtherProjects from './Parts/OtherProjects/otherProjects';
-import Services from './Parts/Services/services';
-import Newsletter from './Parts/Newslatter/newsletter';
-import Portfolio from './Parts/Portfolio/portfolio';
-import CustomerFeedbacks from './Parts/CustomerFeedbacks/customerFeedbacks';
-import Achievements from './Parts/Achievements/achievements';
-import Blog from './Parts/Blog/blog';
-import Footer from './Parts/Footer/footer';
 
 /**
 * Actions
@@ -65,18 +55,7 @@ export const Main = (props) => {
 
     return(
         <div className="main">
-            <Toolbar className={"-main"}/>
-            <MovingBubbles 
-                mainHeader={"Welcome"}
-            />
-            <OtherProjects/>
-            <Services/>
-            <Newsletter/>
-            <Portfolio className="portfolio"/>
-            <CustomerFeedbacks/>
-            <Achievements/>
-            <Blog/>
-            <Footer/>
+            cd
         </div>
     );
 }
