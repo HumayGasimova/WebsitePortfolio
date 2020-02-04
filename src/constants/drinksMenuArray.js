114941b7c061dca4492842fb087f548715f32ef1
Humay <crypto347@gmail.com>
2020-02-04 17:21:21 +0400

    constants updated

---
+++ src/constants/drinksMenuArray.js
@@ -0,0 +1,10 @@
+export const drinksMenuArray = [
+    'STRAWBERRY-BANANA $14.95',
+    'PEANUT BUTTER_APPLE $14.95',
+    'RASPBERRY-ORANGE $14.95',
+    'CHOCOLATE CHIP COOKIE $14.95',
+    'BLACK & WHITE $14.95',
+    'PINEAPPLE-MANGO $14.95',
+    'BANANA PB&J $14.95',
+    'CREAMSICLE $14.95'
+]
